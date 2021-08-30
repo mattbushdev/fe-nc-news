f477fb64f82f4254182559ba82a5815608fc1533
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-08-30 11:23:59 +0100

    added article and comment components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import ArticlesList from "./components/ArticlesList";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Article from "./components/Article";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <ArticlesList />
+          </Route>
+          <Route exact path="/articles/:topic">
+            <ArticlesList />
+          </Route>
+          <Route exact path="/article/:article_id">
+            <Article />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
