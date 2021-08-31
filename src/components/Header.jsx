cce8af7197f4bdd7fa81f933844f9e52c3707d67
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-08-31 17:12:08 +0100

    add user login to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ username }) => {
   return (
-    <header className="title">
-      <Link className="title" to="/">
-        <h1>NC-News</h1>
-      </Link>
+    <header>
+      <div className="login">
+        <Link to={`/user/${username}`}>
+          <button className="button__user">grumpy19</button>
+        </Link>
+      </div>
+      <div className="title">
+        <Link className="title" to="/">
+          <h1>NC-News</h1>
+        </Link>
+      </div>
     </header>
   );
 };
