b25b07e69de33c5e4adefb896ce08d24b98b75ae
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-01 12:15:47 +0100

    add component for posting comment

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { postComment } from "../api";
+
+const AddComment = ({ article_id }) => {
+  const [commentUsername, setCommentUsername] = useState("");
+  const [commentBody, setCommentBody] = useState("");
+  const [hasErrored, setHasErrored] = useState(false);
+
+  const handleUsername = ({ target: { value } }) => {
+    setCommentUsername(value);
+  };
+
+  const handleCommentBody = ({ target: { value } }) => {
+    setCommentBody(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setHasErrored(false);
+
+    postComment(article_id, commentUsername, commentBody).catch(() => {
+      setHasErrored(true);
+    });
+    setCommentUsername("");
+    setCommentBody("");
+  };
+
+  return (
+    <form>
+      <label>
+        <input
+          id="comment-username"
+          type="text"
+          placeholder="username..."
+          onChange={handleUsername}
+          required
+        ></input>
+      </label>
+      <br></br>
+      <label>
+        <textarea
+          id="commentBody"
+          name="message"
+          rows="10"
+          cols="50"
+          maxLength="500"
+          placeholder="type comment..."
+          onChange={handleCommentBody}
+          required
+        ></textarea>
+      </label>
+      {hasErrored && <p>Error, please try again later...</p>}
+      <button
+        type="submit"
+        className="button__post-comment"
+        onClick={handleSubmit}
+      >
+        Post
+      </button>
+    </form>
+  );
+};
+
+export default AddComment;
