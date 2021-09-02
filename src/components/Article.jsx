685941d0227bf043887a2ff708e37e46bf1ebaf6
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-02 15:50:20 +0100

    add icons and images

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -6,6 +6,12 @@ import ExpandAddComment from "./ExpandAddComment";
 import AddComment from "./AddComment";
 import { useVoteCount } from "../hooks/useVoteCount.jsx";
 import { useArticle } from "../hooks/useApi";
+import thumbup from "../icons/thumb-up.svg";
+import thumbdown from "../icons/thumb-down.svg";
+import calendar from "../icons/calendar.svg";
+import coding from "../images/coding.jpg";
+import football from "../images/football.jpg";
+import cooking from "../images/cooking.jpg";
 
 const Article = () => {
   const [commentsList, setCommentsList] = useState([]);
@@ -13,18 +19,25 @@ const Article = () => {
   const { voteCount, incVotes, decVotes, hasErrored } =
     useVoteCount(article_id);
   const { article, isLoading } = useArticle(article_id);
+  const images = { coding, football, cooking };
 
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
   return (
     <section className="articles">
+      <img
+        src={images[article.topic]}
+        alt={`${article.topic} topic`}
+        className="article__img"
+      />
       <Link to={`/articles/${article.topic}`}>
         <button className="button__topic">{article.topic}</button>
       </Link>
       <p className="article__header">
         <span className="span__color-article">{article.author}</span>
         <span className="span__date-article">
+          <img src={calendar} alt="calendar icon" className="calendar" />
           {Date(article.created_at).substring(
             0,
             Date(article.created_at).length - 31
@@ -38,11 +51,11 @@ const Article = () => {
         <p className="votes">
           {hasErrored && <p>Error, please try again later...</p>}
           Votes: {article.votes + voteCount}
-          <button onClick={incVotes} className="button__circle button__green">
-            +
+          <button onClick={incVotes} className="button__thumb-up">
+            <img src={thumbup} alt="thumb up icon to up-vote" />
           </button>
-          <button onClick={decVotes} className="button__circle button__red">
-            -
+          <button onClick={decVotes} className="button__thumb-down">
+            <img src={thumbdown} alt="thumb down icon to down-vote" />
           </button>
         </p>
         <ExpandComments article={article}>
