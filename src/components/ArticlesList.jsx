3874b104e3e343ff9fd46b79119fc782a1ed662c
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-03 09:27:41 +0100

    add images and icons

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,20 +1,25 @@
-import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { getArticles } from "../api";
+import { useArticles } from "../hooks/useApi";
+import coding from "../images/coding.jpg";
+import football from "../images/football.jpg";
+import cooking from "../images/cooking.jpg";
+import calendar from "../icons/calendar.svg";
 
-const ArticlesList = () => {
-  const [articles, setArticles] = useState([]);
+const ArticlesList = ({ user, filters }) => {
   const { topic } = useParams();
+  const { articles, isLoading } = useArticles(filters);
+  const images = { coding, football, cooking };
 
-  useEffect(() => {
-    getArticles(topic).then((articles) => {
-      setArticles(articles);
-    });
-  }, [topic]);
+  const convertTime = (time) => {
+    let date = new Date(time);
+    return (
+      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
+    );
+  };
 
+  if (isLoading) return <h3>Loading...</h3>;
   return (
     <section className="articles">
-      <h2>Top Articles</h2>
       <ul className="articles__list">
         {articles.map((article) => {
           return (
@@ -23,8 +28,31 @@ const ArticlesList = () => {
               key={article.article_id}
             >
               <li className="articles__card">
-                {article.title}
-                <p>{article.author}</p>
+                <div className="container__img">
+                  <img
+                    src={images[article.topic]}
+                    alt={`${article.topic} topic`}
+                  />
+                </div>
+                <div className="container__info">
+                  <p>
+                    <span className="span__color">{article.author}</span>
+                    <span className="span__title">{article.title}</span>
+                    <div className="articles__footer">
+                      <span className="articles__date">
+                        <img
+                          src={calendar}
+                          alt="calendar icon"
+                          className="calendar"
+                        />
+                        {convertTime(article.created_at)}
+                      </span>
+                      <span className="articles__vote">
+                        {article.votes} Votes
+                      </span>
+                    </div>
+                  </p>
+                </div>
               </li>
             </Link>
           );
