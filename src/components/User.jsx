f57e75b2ebb6b49e27fb7d94a37642dd070865e2
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-03 09:32:23 +0100

    add user avatar

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,7 +2,13 @@ const User = ({ user }) => {
   return (
     <section>
       <h2>Hey, {user.username}</h2>
-      <img src={user.avatar_url} alt="user profile"></img>
+      <img
+        src={user.avatar_url}
+        width="200"
+        height="200"
+        alt="user profile"
+      ></img>
+      <br></br>
       <button>Your articles</button>
       <button>Your comments</button>
     </section>
