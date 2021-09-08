478276509cea838001ca66ef34c696bd7d158cfd
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-08 22:00:01 +0100

    add in isLoggedIn functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,29 @@
-const User = ({ user }) => {
+const User = ({ user, isLoggedIn, setIsLoggedIn }) => {
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
-    <section>
+    <section className="user__page">
       <h2>Hey, {user.username}</h2>
-      <img
-        src={user.avatar_url}
-        width="200"
-        height="200"
-        alt="user profile"
-      ></img>
-      <br></br>
-      <button>Your articles</button>
-      <button>Your comments</button>
+      <div className="user__avatar">
+        <img
+          src={user.avatar_url}
+          width="200"
+          height="200"
+          alt="user profile"
+        ></img>
+      </div>
+
+      <div className="user__actions">
+        <button className="user__button">Your articles</button>
+        <button className="user__button">Your comments</button>
+      </div>
+      <div className="user__logout">
+        <button className="user__button-logout" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
     </section>
   );
 };
