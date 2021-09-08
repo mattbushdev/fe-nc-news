dc4e4ea6a5724271c28e60273b35b5199f8937fb
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-08 22:04:22 +0100

    add login functionality and footer

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import Article from "./components/Article";
 import User from "./components/User";
 import { useEffect, useState } from "react";
 import { getUser } from "./api";
+import Login from "./components/Login";
+import Footer from "./components/Footer";
 
 function App() {
   const [user, setUser] = useState({});
-  const [username, setUsername] = useState("grumpy19");
+  const [username, setUsername] = useState("jessjelly");
   const [filters, setFilters] = useState({
     topic: null,
     sort_by: null,
     order: null,
   });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     getUser(username).then((user) => {
@@ -26,30 +29,46 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header username={username} />
-        <Nav filters={filters} setFilters={setFilters} />
-        <Switch>
-          <Route exact path="/user/:username">
-            <User user={user} />
-          </Route>
-          <Route exact path="/">
-            <ArticlesList
-              user={user}
-              filters={filters}
-              setFilters={setFilters}
-            />
-          </Route>
-          <Route exact path="/articles/:topic">
-            <ArticlesList
-              user={user}
-              filters={filters}
-              setFilters={setFilters}
-            />
-          </Route>
-          <Route exact path="/article/:article_id">
-            <Article />
-          </Route>
-        </Switch>
+        <Header username={username} user={user} />
+        {isLoggedIn ? (
+          <>
+            <Nav setFilters={setFilters} />
+            <Switch>
+              <Route exact path="/user/:username">
+                <User
+                  user={user}
+                  isLoggedIn={isLoggedIn}
+                  setIsLoggedIn={setIsLoggedIn}
+                />
+              </Route>
+              <Route exact path="/">
+                <ArticlesList
+                  user={user}
+                  filters={filters}
+                  setFilters={setFilters}
+                />
+              </Route>
+              <Route exact path="/articles/:topic">
+                <ArticlesList
+                  user={user}
+                  filters={filters}
+                  setFilters={setFilters}
+                />
+              </Route>
+              <Route exact path="/article/:article_id">
+                <Article username={username} />
+              </Route>
+            </Switch>
+          </>
+        ) : (
+          <Login
+            username={username}
+            setUsername={setUsername}
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+        )}
+        <Footer />
       </div>
     </BrowserRouter>
   );
